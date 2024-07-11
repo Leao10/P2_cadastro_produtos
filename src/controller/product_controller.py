f4f15ae528e4fdecc14775ea07bc548365259b82
Leao10 <gabriellfrigo@gmail.com>
2024-07-11 20:07:18 -0300

    3 commit

--- src/controller/product_controller.py
+++ src/controller/product_controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Header
 
 from src.repository.usuario_repository import ProductRepository
 from src.config.dependencies import get_authenticated_user, get_product_service
@@ -8,31 +8,26 @@ from src.service.product_service import ProductService
 product_router = APIRouter(prefix='/products', tags=['Products'], dependencies=[Depends(get_authenticated_user)])
 
 
-@product_router.post('/', status_code=201, description='Adiciona um novo produto', response_model=ProdutoCreateDTO)
-async def create(request: ProdutoCreateDTO, service: ProductService = Depends(get_product_service)):
-    product_service = ProductService(service)
+@product_router.post(path='/', status_code=201, description='Adiciona um novo produto', response_model=ProdutoDTO)
+async def create(request: ProdutoCreateDTO, product_service: ProductService = Depends(get_product_service)):
     return product_service.create(request)
 
 
-@product_router.get('/{user_id}', status_code=200, description='Busca o produto pelo id', response_model=ProdutoDTO)
-async def find_by_id(user_id: int, service: ProductService = Depends(get_product_service)):
-    product_service = ProductService(service)
+@product_router.get(path='/{user_id}', status_code=200, description='Busca o produto pelo id', response_model=ProdutoDTO)
+async def find_by_id(user_id: int, product_service: ProductService = Depends(get_product_service)):
     return product_service.find_by_id(user_id=user_id)
 
 
-@product_router.get('/', status_code=200, description='Busca todos os produtos', response_model=list[ProdutoDTO])
-async def find_all(service: ProductService = Depends(get_product_service)):
-    product_service = ProductService(service)
+@product_router.get(path='/', status_code=200, description='Busca todos os produtos', response_model=list[ProdutoDTO])
+async def find_all(product_service: ProductService = Depends(get_product_service)):
     return product_service.find_all()
 
 
-@product_router.put('/{user_id}', status_code=200, description='Atualiza um produto', response_model=ProdutoDTO)
-async def update(user_id: int, user_data: ProdutoUpdateDTO, service: ProductService = Depends(get_product_service)):
-    product_service = ProductService(service)
+@product_router.put(path='/{user_id}', status_code=200, description='Atualiza um produto', response_model=ProdutoDTO)
+async def update(user_id: int, user_data: ProdutoUpdateDTO, product_service: ProductService=Depends(get_product_service)):
     return product_service.update(user_id, user_data)
 
 
-@product_router.delete('/{user_id}', status_code=204, description='Deleta o produto pelo id')
-async def delete(user_id: int, service: ProductService = Depends(get_product_service)):
-    product_service = ProductService(service)
+@product_router.delete(path='/{user_id}', status_code=204, description='Exclui o produto pelo id')
+async def delete(user_id: int, product_service: ProductService = Depends(get_product_service)):
     product_service.delete(user_id=user_id)
--- src/service/product_service.py
+++ src/service/product_service.py
@@ -8,56 +8,49 @@ from src.domain.dto.dtos import ProdutoCreateDTO, ProdutoDTO, ProdutoUpdateDTO
 from src.domain.model.models import Product
 from src.repository.usuario_repository import ProductRepository
 
-logger = logging.getLogger('fastapi')
-
 class ProductService:
 
     def __init__(self, repository: ProductRepository):
         self.repository = repository
 
-    def create(self, user_data: ProdutoCreateDTO) -> ProdutoDTO:
-        logger.info('Criando um novo produto')
-        user = Product(**user_data.model_dump())
+    def create(self, data: ProdutoCreateDTO) -> ProdutoDTO:
+        logging.info('Criando um novo produto')
+        produto = Product(**data.model_dump())
         try:
-            created = self.usuario_repository.save(user)
+            created = self.usuario_repository.save(produto)
             return TypeAdapter(ProdutoDTO).validate_python(created)
         except IntegrityError as e:
-            logger.error(f'Erro na criação o Produto: {user_data.model_dump()}')
+            logger.error(f'Erro na criação o Produto: {data.model_dump()}')
             raise HTTPException(status_code=409, detail=f'Produto já existe: {e.args[0]}')
 
     def read(self, user_id: int) -> ProdutoDTO:
-        logger.info('Buscando produto')
+        logging.info('Buscando produto')
         return TypeAdapter(ProdutoDTO).validate_python(self._read(user_id))
 
     def _read(self, user_id: int) -> Product:
-        user = self.usuario_repository.read(user_id)
-        if user is None:
-            self.logger.error(f'Produto {user_id}, não foi encontrado')
+        produto = self.usuario_repository.read(user_id)
+        if produto is None:
+            logging.error(f'Produto {user_id}, não foi encontrado')
             raise HTTPException(status_code=404, detail=f'Produto {user_id} não encontrado!')
-        return user
+        return produto
 
     def find_all(self) -> list[ProdutoDTO]:
-        logger.info('Buscando todos produtos')
-        users = self.usuario_repository.find_all()
-        return [TypeAdapter(ProdutoDTO).validate_python(user) for user in users]
-
-    def update(self, user_id: int, user_data: ProdutoUpdateDTO) -> ProdutoDTO:
-        logging.info(f'Atualizando produto com ID {user_id}')
-        # TODO: implementar método
-        pass
+        logging.info('Buscando todos produtos')
+        produtos = self.repository.find_all()
+        return [TypeAdapter(ProdutoDTO).validate_python(produto) for produto in produtos]
 
     def update(self, user_id: int, user_data: ProdutoUpdateDTO):
-        logger.info('Atualizando o produto: {user_id}')
-        user = self._read(user_id)
-        user_data = user_data.model_dump(exclude_unset=True)
+        logging.info('Atualizando o produto: {user_id}')
+        produto = self._read(user_id)
+        data = user_data.model_dump(exclude_unset=True)
         for key, value in user_data.items():
-            setattr(user, key, value)
-        user_updated = self.usuario_repository.save(user)
-        logger.info(f'Produto {user_id}, foi atualizado: {user_updated}')
-        return TypeAdapter(ProdutoDTO).validate_python(user_updated)
+            setattr(produto, key, value)
+        produto_updated = self.repository.save(produto)
+        logging.info(f'Produto {user_id}, foi atualizado: {produto_updated}')
+        return TypeAdapter(ProdutoDTO).validate_python(produto_updated)
 
     def delete(self, user_id: int) -> int:
-        user = self._read(user_id)
-        self.usuario_repository.delete(user)
-        logger.info(f'Produto {user_id}, foi deletado')
+        produto = self._read(user_id)
+        self.repository.delete(produto)
+        logging.info(f'Produto {user_id}, foi deletado')
         return user_id
